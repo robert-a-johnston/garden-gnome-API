e129c6e2c6a11307320933fc0c6e78c89ebdc6d0
ar[arnau321 <arnau123@att.net>
2021-09-15 13:44:27 -0400

    Added models and removed example model

---
+++ api/models/seed.py
@@ -0,0 +1,41 @@
+from api.models.garden import Garden
+from django.db import models
+from django.contrib.auth import get_user_model
+from .garden import Garden
+
+# seed model
+class Seed(models.Model):
+  # seed attributes
+  name = models.CharField(max_length=100)
+  type = models.CharField(max_length=100)
+  season = models.CharField(max_length=100)
+  light = models.CharField(max_length=100)
+  favorite = models.BooleanField()
+  number = models.IntegerField()
+  notes = models.TextField(blank=True)
+  image = models.ImageField(blank=True)
+  owner = models.ForeignKey(
+    get_user_model(),
+    on_delete=models.CASCADE
+  )
+  garden = models.ForeignKey(
+    Garden,
+    on_delete=models.CASCADE
+  )
+
+  def __str__(self):
+    # returns info string
+    return f"{self.name}, {self.type}, {self.number}"
+
+  def as_dict(self):
+    """Returns dictionary of Seed"""
+    return {
+      'id': self.id,
+      'name': self.name,
+      'type': self.type,
+      'light': self.light,
+      'favorite': self.favorite,
+      'number': self.number,
+      'notes': self.notes,
+      'image': self.image
+    }
